8a71303a02c088ac081f49a45b9208578d810e04
bahrmichael <mbahr92@gmail.com>
2017-10-03 14:56:20 +0200

    spring boot init

---
+++ src/main/java/de/bahr/eve/corprattingtaxes/CorpRattingTaxesApplication.java
@@ -0,0 +1,12 @@
+package de.bahr.eve.corprattingtaxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpRattingTaxesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpRattingTaxesApplication.class, args);
+	}
+}
